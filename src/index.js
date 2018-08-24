84603ef7453e537bcc25664aa4d5125817cfc4e9
Ara Yeressian <ara_yeressian@epam.com>
2018-08-24 18:06:50 +0400

    add setting schema dynamically

--- src/index.js
+++ src/index.js
@@ -52,16 +52,9 @@ class DBDesigner extends HTMLElement {
     });
   }
 
-  connectedCallback() {
-    console.log('Custom square element added to page.');
-  }
-
-  disconnectedCallback() {
-    console.log('Custom square element removed from page.');
-  }
-
-  adoptedCallback() {
-    console.log('Custom square element moved to new page.');
+  set schema(schema) {
+    const tables = schemaParser(schema);
+    this.designer.load(tables);
   }
 }
 
