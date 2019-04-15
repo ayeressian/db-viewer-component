30b9c2c01a51cbac1469c789781b3a50b552c0d8
ayeressian <ayeressian2@gmail.com>
2019-04-15 11:23:23 +0400

    updated tests

--- test/Relation.test.js
+++ test/Relation.test.js
@@ -20,62 +20,71 @@ describe('Relation', () => {
       Relation.prototype._createHighlightTrigger = sinon.fake();
     });
     describe('Relation.prototype._get2LinePathFlatTop', () => {
-      it('Returns correct result', () => {
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._get2LinePathFlatTop(start, end);
-        expect(Relation.prototype._createPath).to.have.been.calledWith('M 107 95 v 10 M 100 100 H 500 V 500 M 500 500 l 4 -9 M 500 500 l -4 -9');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 H 500 V 500');
+        const argument = 'M 100 100 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 110 100 H 500 V 500 M 500 500 l 4 -9 M 500 500 l -4 -9';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._get2LinePathFlatBottom', () => {
-      it('Returns correct result', () => {
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._get2LinePathFlatBottom(start, end);
-        expect(Relation.prototype._createPath).to.have.been.calledWith('M 95 107 h 10 M 100 100 V 500 H 500 M 500 500 l -9 4 M 500 500 l -9 -4');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 V 500 H 500');
+        const argument = 'M 95 105 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 100 110 V 500 H 500 M 500 500 l -9 4 M 500 500 l -9 -4';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._get3LinePathHoriz', () => {
-      it('Returns correct result', () => {
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._get3LinePathHoriz(start, end);
-        expect(Relation.prototype._createPath).to.have.been.
-          calledWith('M 107 95 v 10 M 100 100 H 300 V 500 H 500 M 500 500 l -9 4 M 500 500 l -9 -4');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 H 300 V 500 H 500');
+        const argument = 'M 100 100 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 100 100 m 10 0 H 300V 500 H 500 M 500 500 l -9 4 M 500 500 l -9 -4';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._get3LinePathVert', () => {
-      it('Returns correct result', () => {
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._get3LinePathVert(start, end);
-        expect(Relation.prototype._createPath).to.have.been.
-          calledWith('M 95 107 h 10 M 100 100 V 300 H 500 V 500 M 500 500 l 4 -9 M 500 500 l -4 -9');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 V 300 H 500 V 500');
+        const argument = 'M 95 105 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 100 110 V 300 H 500 V 500 M 500 500 l 4 -9 M 500 500 l -4 -9';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._getSelfRelationLeft', () => {
-      it('Returns correct result', () => {
+      const end = {x: 100, y: 500};
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._getSelfRelationLeft(start, end);
-        expect(Relation.prototype._createPath).to.have.been.
-          calledWith('M 93 95 v 10M 100 100 h -40 V 500 h 40M 500 500 l -9 -4M 500 500 l -9 4');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 h -40 V 500 h 40');
+        const argument = 'M 90 100 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 90 100 h -30 V 500 h 40 M 100 500 l -9 -4 M 100 500 l -9 4';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._getSelfRelationRight', () => {
-      it('Returns correct result', () => {
+      const end = {x: 100, y: 500};
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._getSelfRelationRight(start, end);
-        expect(Relation.prototype._createPath).to.have.been.calledWith('M 107 95 v 10M 100 100 h 40 V 500 h -40M 500 500 l 9 -4M 500 500 l 9 4');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 h 40 V 500 h -40');
+        const argument = 'M 100 100 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 110 100 h 30 V 500 h -40 M 100 500 l 9 -4 M 100 500 l 9 4';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._getSelfRelationTop', () => {
-      it('Returns correct result', () => {
+      const end = {x: 500, y: 100};
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._getSelfRelationTop(start, end);
-        expect(Relation.prototype._createPath).to.have.been.calledWith('M 95 93 h 10M 100 100 v -40 H 500 v 40M 500 500 l -4 -9M 500 500 l 4 -9');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 v -40 H 500 v 40');
+        const argument = 'M 95 95 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 100 90 v -30 H 500 v 40 M 500 100 l -4 -9 M 500 100 l 4 -9';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
     describe('Relation.prototype._getSelfRelationBottom', () => {
-      it('Returns correct result', () => {
+      const end = {x: 500, y: 100};
+      it('Calls correct methods with correct arguments', () => {
         Relation.prototype._getSelfRelationBottom(start, end);
-        expect(Relation.prototype._createPath).to.have.been.calledWith('M 95 107 h 10M 100 100 v 40 H 500 v -40M 500 500 l -4 9M 500 500 l 4 9');
-        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith('M 100 100 v 40 H 500 v -40');
+        const argument = 'M 95 105 a 1,1 0 1,0 10,0 a 1,1 0 1,0 -10,0 M 100 110 v 30 H 500 v -40 M 500 100 l -4 9 M 500 100 l 4 9';
+        expect(Relation.prototype._createPath).to.have.been.calledWith(argument);
+        expect(Relation.prototype._createHighlightTrigger).to.have.been.calledWith(argument);
       });
     });
   });
