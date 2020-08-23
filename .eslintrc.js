88b2557e67ec5f3bafa60130d250bcbbdf43697f
Ara Yeressian <ayeressian2@gmail.com>
2020-08-23 11:39:00 +0400

    fix versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
@@ -12,14 +13,9 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
+    "prettier/@typescript-eslint",
   ],
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
   rules: {
     "@typescript-eslint/no-non-null-assertion": 0,
     semi: 2,
