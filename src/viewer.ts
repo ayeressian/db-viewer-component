e938d0ef82597057970bbfc4201d5ee77a0180a1
Ara Yeressian <yeressian@tuta.io>
2021-04-24 11:40:19 +0400

    refactor setViewport viewer.ts

--- src/viewer.ts
+++ src/viewer.ts
@@ -384,34 +384,34 @@ export default class Viewer {
         relation.calcPathTableSides()
       );
 
-      const leftRelations = tableRelations.filter(
-        (r) =>
-          ((r.toTable === table && r.toTablePathSide === Orientation.Left) ||
-            (r.fromTable === table &&
-              r.fromTablePathSide === Orientation.Left)) &&
-          !r.sameTableRelation()
-      );
-      const rightRelations = tableRelations.filter(
-        (r) =>
-          ((r.toTable === table && r.toTablePathSide === Orientation.Right) ||
-            (r.fromTable === table &&
-              r.fromTablePathSide === Orientation.Right)) &&
-          !r.sameTableRelation()
-      );
-      const topRelations = tableRelations.filter(
-        (r) =>
-          ((r.toTable === table && r.toTablePathSide === Orientation.Top) ||
-            (r.fromTable === table &&
-              r.fromTablePathSide === Orientation.Top)) &&
-          !r.sameTableRelation()
-      );
-      const bottomRelations = tableRelations.filter(
-        (r) =>
-          ((r.toTable === table && r.toTablePathSide === Orientation.Bottom) ||
-            (r.fromTable === table &&
-              r.fromTablePathSide === Orientation.Bottom)) &&
-          !r.sameTableRelation()
-      );
+      const leftRelations = [] as Relation[],
+        rightRelations = [] as Relation[],
+        topRelations = [] as Relation[],
+        bottomRelations = [] as Relation[];
+
+      for (const tableRelation of tableRelations) {
+        let pathSide: Orientation | undefined;
+        if (tableRelation.sameTableRelation()) continue;
+        if (tableRelation.toTable === table) {
+          pathSide = tableRelation.toTablePathSide;
+        } else if (tableRelation.fromTable === table) {
+          pathSide = tableRelation.fromTablePathSide;
+        }
+        switch (pathSide) {
+          case Orientation.Left:
+            leftRelations.push(tableRelation);
+            break;
+          case Orientation.Right:
+            rightRelations.push(tableRelation);
+            break;
+          case Orientation.Top:
+            topRelations.push(tableRelation);
+            break;
+          case Orientation.Bottom:
+            bottomRelations.push(tableRelation);
+            break;
+        }
+      }
 
       Relation.ySort(leftRelations, table);
       Relation.ySort(rightRelations, table);
@@ -557,6 +557,65 @@ export default class Viewer {
     }
   }
 
+  private setViewportCenterByTableWeight(): {
+    viewportX: number;
+    viewportY: number;
+  } {
+    let totalX = 0;
+    let totalY = 0;
+    const filteredTables = this.tables.filter((table) => {
+      const data = table.data();
+      return data.pos.x >= 0 && data.pos.y >= 0;
+    });
+    filteredTables.forEach((table) => {
+      const data = table.data();
+      totalX += data.pos.x + data.width / 2;
+      totalY += data.pos.y + data.height / 2;
+    });
+    const centerX = totalX / filteredTables.length;
+    const centerY = totalY / filteredTables.length;
+    const viewportX = centerX - this.viewBoxVals.width / 2;
+    const viewportY = centerY - this.viewBoxVals.height / 2;
+    return { viewportX, viewportY };
+  }
+
+  private setViewportCenter(): { viewportX: number; viewportY: number } {
+    const width = this.svgContainer.clientWidth;
+    const height = this.svgContainer.clientHeight;
+    const viewportX = this.viewWidth / 2 - width / 2;
+    const viewportY = this.viewHeight / 2 - height / 2;
+    return { viewportX, viewportY };
+  }
+
+  private setViewportCenterByTables(): {
+    viewportX: number;
+    viewportY: number;
+  } {
+    if (this.tables.length === 0) {
+      return this.setViewportCenter();
+    }
+    let minX = Number.MAX_SAFE_INTEGER;
+    let minY = Number.MAX_SAFE_INTEGER;
+    let maxX = Number.MIN_SAFE_INTEGER;
+    let maxY = Number.MIN_SAFE_INTEGER;
+    this.tables.forEach((table) => {
+      const data = table.data();
+      if (data.pos.x >= 0 && data.pos.y >= 0) {
+        if (data.pos.x < minX) minX = data.pos.x;
+        if (data.pos.y < minY) minY = data.pos.y;
+        if (data.pos.x + data.width > maxX) maxX = data.pos.x + data.width;
+        if (data.pos.y + data.height > maxY) maxY = data.pos.y + data.height;
+      }
+    });
+    const centerX = (minX + maxX) / 2;
+    const centerY = (minY + maxY) / 2;
+    let viewportX = centerX - this.viewBoxVals.width / 2;
+    let viewportY = centerY - this.viewBoxVals.height / 2;
+    if (viewportX < 0) viewportX = 0;
+    if (viewportY < 0) viewportY = 0;
+    return { viewportX, viewportY };
+  }
+
   setViewport(type = Viewport.noChange): Promise<[void, void]> {
     let viewportX;
     let viewportY;
@@ -564,60 +623,14 @@ export default class Viewer {
       case Viewport.noChange:
         return Promise.resolve([undefined, undefined]);
       case Viewport.centerByTablesWeight:
-        {
-          let totalX = 0;
-          let totalY = 0;
-          const filteredTables = this.tables.filter((table) => {
-            const data = table.data();
-            return data.pos.x >= 0 && data.pos.y >= 0;
-          });
-          filteredTables.forEach((table) => {
-            const data = table.data();
-            totalX += data.pos.x + data.width / 2;
-            totalY += data.pos.y + data.height / 2;
-          });
-          const centerX = totalX / filteredTables.length;
-          const centerY = totalY / filteredTables.length;
-          viewportX = centerX - this.viewBoxVals.width / 2;
-          viewportY = centerY - this.viewBoxVals.height / 2;
-        }
+        ({ viewportX, viewportY } = this.setViewportCenterByTableWeight());
         break;
       case Viewport.center:
-        {
-          const width = this.svgContainer.clientWidth;
-          const height = this.svgContainer.clientHeight;
-          viewportX = this.viewWidth / 2 - width / 2;
-          viewportY = this.viewHeight / 2 - height / 2;
-        }
+        ({ viewportX, viewportY } = this.setViewportCenter());
         break;
       default:
         // centerByTables
-        {
-          if (this.tables.length === 0) {
-            return this.setViewport(Viewport.center);
-          }
-          let minX = Number.MAX_SAFE_INTEGER;
-          let minY = Number.MAX_SAFE_INTEGER;
-          let maxX = Number.MIN_SAFE_INTEGER;
-          let maxY = Number.MIN_SAFE_INTEGER;
-          this.tables.forEach((table) => {
-            const data = table.data();
-            if (data.pos.x >= 0 && data.pos.y >= 0) {
-              if (data.pos.x < minX) minX = data.pos.x;
-              if (data.pos.y < minY) minY = data.pos.y;
-              if (data.pos.x + data.width > maxX)
-                maxX = data.pos.x + data.width;
-              if (data.pos.y + data.height > maxY)
-                maxY = data.pos.y + data.height;
-            }
-          });
-          const centerX = (minX + maxX) / 2;
-          const centerY = (minY + maxY) / 2;
-          viewportX = centerX - this.viewBoxVals.width / 2;
-          viewportY = centerY - this.viewBoxVals.height / 2;
-          if (viewportX < 0) viewportX = 0;
-          if (viewportY < 0) viewportY = 0;
-        }
+        ({ viewportX, viewportY } = this.setViewportCenterByTables());
         break;
     }
     return Promise.all([this.setPanX(viewportX), this.setPanY(viewportY)]);
