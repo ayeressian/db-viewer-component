3cbda4f9eb4830e39fed67696f077ad4a974ce0d
Ara Yeressian <yeressian@tuta.io>
2021-07-25 00:27:53 +0400

    test: add basic e2e tests for annotations

--- e2e/school.test.ts
+++ e2e/school.test.ts
@@ -7,45 +7,68 @@ import { getNumberOfRelations, getNumberOfTables } from "./util";
 
 const { describe, beforeEach } = test;
 
-describe("table", () => {
+describe("school example", () => {
   beforeEach(async ({ page }) => {
     await page.goto("http://localhost:9998");
     await page.waitForLoadState("domcontentloaded");
   });
 
-  test("it should render correct number of tables", async ({ page }) => {
-    const tables = await page.$$("#school-db table");
-    expect(tables.length).toBe(getNumberOfTables(schoolSchema));
+  describe("relations", () => {
+    test("it should show correct number of relations", async ({ page }) => {
+      const paths = await page.$$("#school-db path.highlight");
+      expect(paths.length).toBe(getNumberOfRelations(schoolSchema));
+    });
   });
 
-  test("should show correct number of relations", async ({ page }) => {
-    const paths = await page.$$("#school-db path.highlight");
-    expect(paths.length).toBe(getNumberOfRelations(schoolSchema));
-  });
+  describe("annotations", () => {
+    test("it should render correct number of annotations", async ({ page }) => {
+      const annotations = await page.$$("#school-db g.annotation");
+      expect(annotations.length).toBe(schoolSchema.annotations.length);
+    });
 
-  describe("school table", () => {
-    const schoolTableSchema = (schoolSchema as Schema).tables[0];
     test("should have correct title", async ({ page }) => {
-      const tableHeader = await page.$("#school-db #\\30table th");
-      const title = await tableHeader!.innerHTML();
-      expect(title).toBe(schoolTableSchema.name);
+      const h1 = await page.$("#school-db g.annotation h1");
+      expect(await h1?.innerText()).toBe(schoolSchema.annotations[0].title);
     });
 
-    test("should have correct number of rows (column representation)", async ({
-      page,
-    }) => {
-      const tableRows = await page.$$("#school-db #\\30table tr");
-      // minus 1 because of title
-      expect(tableRows.length - 1).toBe(schoolTableSchema.columns.length);
+    test("should have correct text", async ({ page }) => {
+      const p = await page.$("#school-db g.annotation p");
+      expect(await p?.innerText()).toBe(schoolSchema.annotations[0].text);
+    });
+  });
+
+  describe("tables", () => {
+    test("it should render correct number of tables", async ({ page }) => {
+      const tables = await page.$$("#school-db table");
+      expect(tables.length).toBe(getNumberOfTables(schoolSchema));
     });
 
-    test("should have correct cordinates", async ({ page }) => {
-      const foreignObject = await page.$("#school-db #\\30table foreignObject");
-      const pos = schoolTableSchema.pos as Point;
-      const xAttr = await foreignObject!.getAttribute("x");
-      const yAttr = await foreignObject!.getAttribute("y");
-      expect(Number(xAttr)).toBe(pos.x);
-      expect(Number(yAttr)).toBe(pos.y);
+    describe("school table", () => {
+      const schoolTableSchema = (schoolSchema as Schema).tables[0];
+      test("should have correct title", async ({ page }) => {
+        const tableHeader = await page.$("#school-db #\\30table th");
+        const title = await tableHeader!.innerHTML();
+        expect(title).toBe(schoolTableSchema.name);
+      });
+
+      test("should have correct number of rows (column representation)", async ({
+        page,
+      }) => {
+        const tableRows = await page.$$("#school-db #\\30table tr");
+        // minus 1 because of title
+        expect(tableRows.length - 1).toBe(schoolTableSchema.columns.length);
+      });
+
+      test("should have correct cordinates", async ({ page }) => {
+        const foreignObject = await page.$(
+          "#school-db #\\30table foreignObject"
+        );
+        const pos = schoolTableSchema.pos as Point;
+        const xAttr = await foreignObject!.getAttribute("x");
+        const yAttr = await foreignObject!.getAttribute("y");
+        expect(Number(xAttr)).toBe(pos.x);
+        expect(Number(yAttr)).toBe(pos.y);
+      });
     });
   });
 });
